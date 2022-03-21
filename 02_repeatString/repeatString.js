008ac0cd3beb01282b358b75fd21e159f7c5a6bc
IsabelleAnn <isabelle.ann85@gmail.com>
2022-03-21 15:28:12 -0500

    write function that repeats string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let newStr = '';
+    for (let i = 1; i <= num; i++) {
+        newStr += str
+    }
+    return newStr;
 };
+console.log(repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
