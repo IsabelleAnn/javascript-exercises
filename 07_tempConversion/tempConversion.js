9733fbf23132617636b54b6cef0a2bb3472346a1
IsabelleAnn <isabelle.ann85@gmail.com>
2022-03-22 17:30:44 -0500

    write 2 functions that convert C to F temps and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+    return parseFloat(((fTemp - 32) / 1.8).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+    return parseFloat(((cTemp * 1.8) + 32).toFixed(1));
 };
-
+console.log("Celsius to F:");
+console.log(ctof(-10));
+console.log("F to C:");
+console.log(ftoc(32));
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
