c77ee086b5e500c08a6d67a01afb5f93007edfd1
IsabelleAnn <isabelle.ann85@gmail.com>
2022-03-22 16:32:01 -0500

    write function that gets 2 numbers and returns sum of all numbers between, inclusive, with errors for input

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        console.log("ERROR");
+        return "ERROR";
+    }
+    console.log(num1, num2);
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+        console.log(num1, num2);
 
+    }
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    console.log(sum);
+    return sum;
 };
-
+sumAll(3488, 50);
 // Do not edit below this line
 module.exports = sumAll;
