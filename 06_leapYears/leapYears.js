9732e3affecd40ef6d15c5bee8c417cb4c1225e1
IsabelleAnn <isabelle.ann85@gmail.com>
2022-03-22 17:05:11 -0500

    write function that determines if year is leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            console.log("True Leap Year!");
+            return true;
+        }
+        console.log("False Not A Leap Year");
+        return false;
+    }
+    if (year % 4 == 0) {
+        console.log("True Leap Year!");
+        return true;
+    }
+    console.log("False Not A Leap Year");
+    return false;
 };
+leapYears(1996); //true OK
+leapYears(1997); //false OK
+leapYears(34992); //true OK
+leapYears(1900); //false WRONG
+leapYears(1600); //true WRONG
+leapYears(700); //false WRONG
+
 
 // Do not edit below this line
 module.exports = leapYears;
