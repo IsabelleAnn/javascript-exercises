930316532c85c09847799071a032a24bc5f1805f
IsabelleAnn <isabelle.ann85@gmail.com>
2022-03-22 16:07:25 -0500

    write function that removes any type of item from an array and returns edited array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    console.log(array);
 
+    let remove = [...args];
+
+    console.log(remove);
+    removeArray:
+        for (let i = 0; i < remove.length; i++) {
+            if (array.indexOf(remove[i]) == -1) {
+                continue removeArray;
+            }
+            array.splice(array.indexOf(remove[i]), 1);
+        }
+    console.log(array);
+    return array;
 };
-
+removeFromArray([1, 2, "ho", 4], 5, "1", "4", "2", "ho", 1);
 // Do not edit below this line
 module.exports = removeFromArray;
